839c2b0bde11f8b5f46f2646c6c0c707ad9ad102
Jack Scott <jack.rob.scott@gmail.com>
2016-09-22 10:03:42 +0800

    remove other unnecessary components

--- server/config/express.js
+++ server/config/express.js
@@ -12,9 +12,6 @@ module.exports = function(app, config) {
   var env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
-  
-  app.set('views', config.root + '/app/views');
-  app.set('view engine', 'jade');
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
   app.use(logger('dev'));
@@ -24,7 +21,6 @@ module.exports = function(app, config) {
   }));
   app.use(cookieParser());
   app.use(compress());
-  app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
@@ -37,7 +33,7 @@ module.exports = function(app, config) {
     err.status = 404;
     next(err);
   });
-  
+
   if(app.get('env') === 'development'){
     app.use(function (err, req, res, next) {
       res.status(err.status || 500);
@@ -57,5 +53,4 @@ module.exports = function(app, config) {
         title: 'error'
       });
   });
-
 };
