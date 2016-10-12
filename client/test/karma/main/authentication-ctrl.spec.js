47e79930aeb652aa1faaaccc548089f41b8e069d
ancilt26 <ancilt26@gmail.com>
2016-10-12 13:26:27 +0800

    Debugged Karma errors

--- client/test/karma/main/authentication-ctrl.spec.js
+++ client/test/karma/main/authentication-ctrl.spec.js
@@ -1,9 +1,31 @@
 'use strict';
 
-// Be descriptive with titles here. The describe and it titles combined read like a sentence.
-describe('Users factory', function() {
-  it('has a dummy spec to test 2 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
-    expect(2 + 2).toEqual(4);
+describe('conversations module', function() {
+
+  beforeEach(angular.mock.module('main'));
+
+  var $controller;
+
+  beforeEach(angular.mock.inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  describe('authentication controller', function() {
+
+    var AuthController;
+
+    beforeEach(function() {
+      AuthController = $controller('AuthController');
+    });
+
+      /*-----SPECS-------*/
+    it('should exist', function() {
+      expect(AuthController).toBeDefined();
+    });
+
+    it('should have login function', function() {
+      expect(AuthController.login).toBeDefined();
+    });
   });
+
 });
