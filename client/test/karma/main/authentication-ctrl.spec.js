9cc07ae0bdbb50dd8eba51413823c4bb8049da79
ancilt26 <ancilt26@gmail.com>
2016-10-12 09:42:46 +0800

    Testing

--- client/test/karma/main/authentication-ctrl.spec.js
+++ client/test/karma/main/authentication-ctrl.spec.js
@@ -1,20 +1,9 @@
 'use strict';
 
-describe('module: main, controller: AuthenticationCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('main'));
-  // load all the templates to prevent unexpected $http requests from ui-router
-  beforeEach(module('ngHtml2Js'));
-
-  // instantiate controller
-  var AuthenticationCtrl;
-  beforeEach(inject(function ($controller) {
-    AuthenticationCtrl = $controller('AuthenticationCtrl');
-  }));
-
-  it('should do something', function () {
-    expect(!!AuthenticationCtrl).toBe(true);
+// Be descriptive with titles here. The describe and it titles combined read like a sentence.
+describe('Users factory', function() {
+  it('has a dummy spec to test 2 + 2', function() {
+    // An intentionally failing test. No code within expect() will never equal 4.
+    expect(2 + 2).toEqual(4);
   });
-
 });
